0eac95e59e0512638cbe241e3268ee1ba53643d7
chetan-redkivi <cheyan.tomar@redkivi.com>
2014-02-24 19:20:33 +0530

    Begin added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @current_selected_user = current_user
     if @current_selected_user.present?
+
       fetch_google_plus_timeline(@current_selected_user)
       fetch_facebook_timeline(@current_selected_user)
       fetch_twitter_timeline(@current_selected_user)
@@ -11,41 +12,53 @@ class HomeController < ApplicationController
 
 
   def fetch_twitter_timeline(current_selected_user)
-    if current_selected_user and current_selected_user.twitter_auth.present?
-      if !current_selected_user.twitter_auth.time_line_statuses.present?
-        client = Twitter::Client.new(oauth_token: current_selected_user.twitter_auth.token, oauth_token_secret: current_selected_user.twitter_auth.secret)
-        @tweets = client.user_timeline(page: 1, count: 100)
-        if @tweets.present?
-          @tweets.each do |tweet|
-            TimeLineStatus.create(status: tweet.text, authentication_id: current_selected_user.twitter_auth.id)
+    begin
+      if current_selected_user and current_selected_user.twitter_auth.present?
+        if !current_selected_user.twitter_auth.time_line_statuses.present?
+          client = Twitter::Client.new(oauth_token: current_selected_user.twitter_auth.token, oauth_token_secret: current_selected_user.twitter_auth.secret)
+          @tweets = client.user_timeline(page: 1, count: 100)
+          if @tweets.present?
+            @tweets.each do |tweet|
+              TimeLineStatus.create(status: tweet.text, authentication_id: current_selected_user.twitter_auth.id)
+            end
           end
         end
       end
+    rescue Exception => e
+      puts "========== Error: #{e.message}"
     end
   end
 
   def fetch_facebook_timeline(current_selected_user)
-    if current_selected_user and current_selected_user.facebook_auth.present?
-      get_fb_graph_api_object(current_selected_user.facebook_auth.token)
-      get_user_statues_details(current_selected_user.facebook_auth.uid, current_selected_user)
+    begin
+      if current_selected_user and current_selected_user.facebook_auth.present?
+        get_fb_graph_api_object(current_selected_user.facebook_auth.token)
+        get_user_statues_details(current_selected_user.facebook_auth.uid, current_selected_user)
+      end
+    rescue Exception => e
+      puts "===========Error #{e.message}"
     end
   end
 
   def fetch_google_plus_timeline(current_selected_user)
-    if current_selected_user and current_selected_user.google_plus_auth.present?
-
-      if !current_selected_user.google_plus_auth.time_line_statuses.present?
-        person = GooglePlus::Person.get(current_selected_user.google_plus_auth.uid)
-        if person.present? && person.list_activities.present? && person.list_activities.items.present?
-          @google_posts = person.list_activities.items
-        end
-        if @google_posts.present?
-          GooglePlus.api_key = GOOGLE_API_KEY
-          @google_posts.each do |post|
-            TimeLineStatus.create(status: post.title, authentication_id: current_selected_user.google_plus_auth.id)
+    begin
+      if current_selected_user and current_selected_user.google_plus_auth.present?
+        if !current_selected_user.google_plus_auth.time_line_statuses.present?
+          person = GooglePlus::Person.get(current_selected_user.google_plus_auth.uid)
+          if person.present? && person.list_activities.present? && person.list_activities.items.present?
+            @google_posts = person.list_activities.items
+          end
+          if @google_posts.present?
+            GooglePlus.api_key = GOOGLE_API_KEY
+            @google_posts.each do |post|
+              TimeLineStatus.create(status: post.title, authentication_id: current_selected_user.google_plus_auth.id)
+            end
           end
         end
       end
+
+    rescue Exception => e
+      puts "============= ERROR: #{e.message}"
     end
   end
 
